d9e1febf3c30e5ef9814c198f86d17266051ad01
TerminaTerrier <ianmg211101@gmail.com>
2024-04-05 18:48:04 -0500

    feat: add basic player controller functionality

---
+++ scripts/player.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+
+	public override void _Ready()
+	{
+		
+	}
+
+}
---
+++ scripts/player_controller.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class player_controller : Node2D
+{
+	public Vector2 direction;
+	
+	public override void _Ready()
+	{
+
+	}
+
+    public override void _Process(double delta)
+    {
+        if(Input.IsActionPressed("InputUp"))
+		{
+			direction = Vector2.Up;
+		}
+
+		if(Input.IsActionPressed("InputDown"))
+		{
+			direction = Vector2.Down;
+		}
+
+		if(Input.IsActionPressed("InputLeft"))
+		{
+			direction = Vector2.Left;
+		}
+
+		if(Input.IsActionPressed("InputRight"))
+		{
+			direction = Vector2.Right;
+		}
+		
+		
+    }
+
+
+
+
+}
